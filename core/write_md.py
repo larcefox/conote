8e5be7cd9c8f6b99a38f230dba645212e3084d05
Larce Fox <larcethefox@gmail.com>
2021-11-21 23:39:14 +0300

    Header works, need template tag

--- core/write_md.py
+++ core/write_md.py
@@ -11,11 +11,12 @@ import datetime
 class MakeFile():
     def __init__(self, text, config):
        self.text = text
-       self.conote_dir = config['FILE']['conote_dir'] 
-       self.conote_file = '/'.join([self.conote_dir, config['FILE']['conote_file']])
-       self.current_time = datetime.datetime.now()
-       self.template_file = config['FILE']['template_file']
-       self.template_event = config['FILE']['template_event']
+       self.config = config
+       self.conote_dir = self.config['FILE']['conote_dir'] 
+       self.conote_file = '/'.join([self.conote_dir, self.config['FILE']['conote_file']])
+       self.date_time = datetime.datetime.now()
+       self.template_file = self.config['FILE']['template_file']
+       self.template_event = self.config['FILE']['template_event']
 
     @property
     def dir_check(self) -> bool:
@@ -34,26 +35,34 @@ class MakeFile():
     def file_check(self) -> bool:
         '''Checks existing conote current day file, specified in config/config.ini'''
         if os.path.isfile(self.conote_file):
-           print(self.conote_file)
            return True
         else:
             try:
-                with open(self.conote_file, 'a') as conote_f:
+                # breakpoint()
+                with open(self.conote_file, 'w') as conote_f:
                     with open(self.template_file, 'r') as template_f:
-                        conote_f.write(template_f.read())
-                        print(template_f.read())
-                return True
+                        header = template_f.read()
+                        conote_f.write(header)
             except OSError as e:
                 print(e)
                 return False
+            return True
 
+    # def tag_dict(self):
+        # for line in str(os.system('lsb_release -a')):
+            # if 'Description:' in line:
+                # dist = line.split()[1]
+                # print(dist)
+    # TODO Need to create template tags interpretation
     def file_write(self) -> None:
         '''Write text in file'''
+        # breakpoint()
         if self.dir_check and self.file_check:
             try:
                 with open(self.conote_file, 'a') as conote_f:
                     with open(self.template_event, 'r') as event_template_f:
                         conote_f.write(event_template_f.read())
+                        self.tag_dict()
                         conote_f.write(''.join(['> ', self.text]))
             except OSError as e:
                 print(e)
