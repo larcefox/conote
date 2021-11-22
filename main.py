584d43659d590fc28e4296f56575cd6418ed284a
Larce Fox <larcethefox@gmail.com>
2021-11-22 23:13:46 +0300

    Start to make tags dict

--- main.py
+++ main.py
@@ -8,10 +8,19 @@ __copyright__   = "GPLv3"
 import argparse
 from core.write_md import MakeFile 
 import configparser
-
+from datetime import datetime 
 
 config = configparser.ConfigParser()
 config.read('./config/config.ini')
+
+tag_date = datetime.now().strftime('%y%m%d')
+tag_time =  datetime.now().strftime('%H:%M')
+
+template_tags = {
+        config['TAGS']['current_date'] : tag_date,
+        config['TAGS']['current_time'] : tag_time}
+
+print(template_tags)
 
 parser = argparse.ArgumentParser(description='Making notes from console.')
 parser.add_argument('-f', dest='message', action='store_const', 
